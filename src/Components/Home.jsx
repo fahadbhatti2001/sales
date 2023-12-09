4699a92acb51fbc408f074e389af3f5d434eb1e7
Fahad Bhatti <83200973+fahadbhatti2001@users.noreply.github.com>
2023-12-09 23:55:26 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Header, Footer } from '@/Components'
-
 const headerLinks = [
   {
     name: "Home", path: "/"
